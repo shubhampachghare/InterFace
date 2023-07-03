f08b4afe5ebe3bceec16fd8222e22d04045d0b66
shubhampachghare <shubhampachghare@gmail.com>
2023-07-04 02:17:05 +0530

    [Shubham] -Examples

---
+++ InterFace/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFace
+{
+     interface IAnimal
+     {
+        void AnimalSound(); //does not have body
+     }
+
+     class Pig : IAnimal 
+     {
+        public void AnimalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+     }
+
+}
---
+++ InterFace/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InterFace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig pig = new Pig();
+            pig.AnimalSound();
+        }
+    }
+}
